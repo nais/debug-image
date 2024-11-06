e02dd61f0b9ca580a419c5e2f21ff6c9fae0efbb
carl hedgren <carl.hedgren@nav.no>
2024-11-06 10:17:29 +0100

    Define --rcfile
    shouldn't --interactive already souce this?

--- flake.nix
+++ flake.nix
@@ -105,7 +105,7 @@
             };
             config = {
               Workingdir = "/home/nais";
-              Entrypoint = [ "bash" ];
+              Entrypoint = [ "bash" "--rcfile" "/etc/bash/bashrc" ];
               User = "1069";
             };
           };
